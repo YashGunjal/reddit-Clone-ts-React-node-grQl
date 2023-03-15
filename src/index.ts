b513f5411145bb6cb1b4950d1c20417d5db3d5ad
Yash Gunjal <yashgunjal98@gmail.com>
2023-03-14 20:15:24 -0400

    Graphql initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,54 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants"
-import { Post } from "./entities/post";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/post";
 import microConfig from "./mikro-orm.config";
 
+import Express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
+const main = async () => {
+  // setting up Express
+  const app = Express();
 
-const main = async() => {
-    const orm  =await  MikroORM.init(microConfig);
+  // app.get("/", (_ ,res)=>{
+  //     res.send("hey");
+  // })
 
-    const post = orm.em.create(Post, {
-        title: "my first post",
-        createdAt: "",
-        updatedAt: ""
-    });
-    await orm.em.persistAndFlush(post);
- 
-}
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  // added this line
+  await apolloServer.start();
 
-main()
+  apolloServer.applyMiddleware({ 
+    app
+    // ,cors:false 
+ });
+
+  app.listen(4000, () => {
+    console.log(" listing onport 4000");
+  });
+
+  // orm setup
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  // Orm setup initial tests
+  // const post = orm.em.create(Post, {
+  //     title: "my first post",
+  //     createdAt: "",
+  //     updatedAt: ""
+  // });
+
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
+};
+
+main();
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { MikroORM } from "@mikro-orm/postgresql"
 import { __prod__ } from "./constants"
 import { Post } from "./entities/post"
+import path  from "path";
 
 export default {
     entities: [Post],
@@ -8,9 +9,10 @@ export default {
     type: "postgresql",
     user:"postgres",
     password:"qazwsxedc",
+    allowGlobalContext:true,
     debug: !__prod__,
     migrations:{
-        path: './migrations', // path to the folder with migrations
+        path: path.join(__dirname,"./migrations"), // path to the folder with migrations
         glob: '!(*.d).{js,ts}'
     }
 } as Parameters< typeof MikroORM.init>[0] ;
